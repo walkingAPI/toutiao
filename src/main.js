0debc0ec388983c860ff38da804a410b5e28e667
王生生 <448196645@qq.com>
2022-05-12 16:27:31 +0800

    第一次上传文件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
 Vue.config.productionTip = false
 
+// 自动设置 Rem 基准值（ html 标签字体大小）
+import 'amfe-flexible'
+
+Vue.use(Vant);
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
